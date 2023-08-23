caac9516708b33c8ee6d04e2fb966f5df215a3df
peyrin <peyrin@berkeley.edu>
2023-08-23 00:22:35 -0700

    Initial commit

---
+++ lec11_inheritance4/TestArraySetEquals.java
@@ -0,0 +1,61 @@
+package lec11_inheritance4;
+
+import org.junit.jupiter.api.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+
+
+/**
+ * Created by hug.
+ */
+public class TestArraySetEquals {
+    @Test
+    public void testThreeEquals() {
+        ArraySet<Integer> aset = new ArraySet<>();
+        aset.add(5);
+        aset.add(23);
+        aset.add(42);
+
+        //equals
+        ArraySet<Integer> aset2 = new ArraySet<>();
+        aset2.add(5);
+        aset2.add(23);
+        aset2.add(42);
+
+       assertThat(aset).isEqualTo(aset2);
+    }
+
+    @Test
+    public void testNotEquals1() {
+        ArraySet<Integer> aset = new ArraySet<>();
+        aset.add(5);
+        aset.add(23);
+        aset.add(42);
+        aset.add(36);
+
+        //equals
+        ArraySet<Integer> aset2 = new ArraySet<>();
+        aset2.add(5);
+        aset2.add(23);
+        aset2.add(42);
+
+        assertThat(aset).isNotEqualTo(aset2);
+    }
+
+    @Test
+    public void testNotEquals2() {
+        ArraySet<Integer> aset = new ArraySet<>();
+        aset.add(5);
+        aset.add(23);
+        aset.add(42);
+
+        //equals
+        ArraySet<Integer> aset2 = new ArraySet<>();
+        aset2.add(5);
+        aset2.add(23);
+        aset2.add(42);
+        aset2.add(36);
+
+        assertThat(aset).isNotEqualTo(aset2);
+    }
+}
---
+++ lec6_testing/TestingPractice.java
@@ -0,0 +1,15 @@
+package lec6_testing;
+
+
+import org.junit.Test;
+
+/** Write a testing method that will take in 2 arrays and see if they are equal.
+ * These arrays can have nested arrays and 
+ * those nested arrays can have nested arrays and so forth. */
+public class TestingPractice {
+    /** Test the Sort.sort method. */
+    @Test
+    public void test(int[] arr1, int[] arr2) {
+    	
+    }
+}

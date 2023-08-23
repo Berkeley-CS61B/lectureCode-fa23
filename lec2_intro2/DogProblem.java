9625a6f773d28e101cc95f6650ef7cb563600ee6
peyrin <peyrin@berkeley.edu>
2023-08-23 00:22:43 -0700

    [lec02] allow nulls in DogProblem

---
+++ lec2_intro2/DogProblem.java
@@ -0,0 +1,37 @@
+package lec2_intro2;
+
+/**
+ * Created by hug.
+ */
+public class DogProblem {
+    public static Dog[] largerThanFourNeighbors(Dog[] dogs) {
+        return dogs;
+    }
+
+    public static void main(String[] args) {
+        Dog[] dogs = new Dog[]{
+                new Dog(10),
+                new Dog(15),
+                new Dog(20),
+                new Dog(15),
+                new Dog(10),
+                new Dog(5),
+                new Dog(10),
+                new Dog(15),
+                new Dog(22),
+                new Dog(15),
+                new Dog(20)
+        };
+        Dog[] bigDogs1 = largerThanFourNeighbors(dogs);
+
+        /** When you run this program you should get "20 22" */
+        for (int k = 0; k < bigDogs1.length; k += 1) {
+            if (bigDogs1[k] != null) { // FA23: added this check to avoid needing arrayWithNoNulls in solution ~peyrin
+                System.out.print(bigDogs1[k].weightInPounds + " ");
+            }
+        }
+
+        System.out.println();
+
+    }
+}

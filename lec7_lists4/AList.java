12770601c5f80a8b242e5179c0833f5fe19bb1a8
peyrin <peyrin@berkeley.edu>
2024-09-09 11:46:28 -0700

    [lec07] blank AList

--- lec7_lists4/AList.java
+++ lec7_lists4/AList.java
@@ -4,63 +4,35 @@ package lec7_lists4;
  *  @author Josh Hug
  */
 
-//         0 1  2 3 4 5 6 7
-// items: [6 9 -1 2 0 0 0 0 ...]
-// size: 5
-
-/* Invariants:
- addLast: The next item we want to add, will go into position size
- getLast: The item we want to return is in position size - 1
- size: The number of items in the list should be size.
-*/
-
-public class AList<Item> {
-    private Item[] items;
-    private int size;
+public class AList {
 
     /** Creates an empty list. */
     public AList() {
-        items = (Item[]) new Object[100];
-        size = 0;
-    }
 
-    /** Resizes the underlying array to the target capacity. */
-    private void resize(int capacity) {
-        Item[] a = (Item[]) new Object[capacity];
-        System.arraycopy(items, 0, a, 0, size);
-        items = a;
     }
 
     /** Inserts X into the back of the list. */
-    public void addLast(Item x) {
-        if (size == items.length) {
-            resize(size + 1);
-        }
+    public void addLast(int x) {
 
-        items[size] = x;
-        size = size + 1;
     }
 
     /** Returns the item from the back of the list. */
-    public Item getLast() {
-        return items[size - 1];
+    public int getLast() {
+        return 0;
     }
     /** Gets the ith item in the list (0 is the front). */
-    public Item get(int i) {
-        return items[i];
+    public int get(int i) {
+        return 0;
     }
 
     /** Returns the number of items in the list. */
     public int size() {
-        return size;
+        return 0;
     }
 
     /** Deletes item from back of the list and
       * returns deleted item. */
-    public Item removeLast() {
-        Item x = getLast();
-        items[size - 1] = null;
-        size = size - 1;
-        return x;
+    public int removeLast() {
+        return 0;
     }
 } 

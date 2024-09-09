de509ba8bd1405909c5eb7828646770c3292928b
peyrin <peyrin@berkeley.edu>
2024-09-09 11:40:44 -0700

    [lec05] blank - non-generic SLList

--- lec5_lists3/SLList.java
+++ lec5_lists3/SLList.java
@@ -2,48 +2,48 @@ package lec5_lists3;
 
 /** An SLList is a list of integers, which hides the terrible truth
    * of the nakedness within. */
-public class SLList<LochNess> {	
-	private class StuffNode {
-		public LochNess item;
-		public StuffNode next;
+public class SLList {	
+	private class IntNode {
+		public int item;
+		public IntNode next;
 
-		public StuffNode(LochNess i, StuffNode n) {
+		public IntNode(int i, IntNode n) {
 			item = i;
 			next = n;
 		}
 	} 
 
-	private StuffNode first;
+	private IntNode first;
 	private int size;
 
-	public SLList(LochNess x) {
-		first = new StuffNode(x, null);
+	public SLList(int x) {
+		first = new IntNode(x, null);
 		size = 1;
 	}
 
  	/** Adds x to the front of the list. */
- 	public void addFirst(LochNess x) {
- 		first = new StuffNode(x, first);
+ 	public void addFirst(int x) {
+ 		first = new IntNode(x, first);
  		size += 1;
  	}
 
  	/** Returns the first item in the list. */
- 	public LochNess getFirst() {
+ 	public int getFirst() {
  		return first.item; 		
  	}
 
  	/** Adds an item to the end of the list. */
- 	public void addLast(LochNess x) {
+ 	public void addLast(int x) {
  		size += 1;
 
- 		StuffNode p = first;
+ 		IntNode p = first;
 
  		/* Move p until it reaches the end of the list. */
  		while (p.next != null) {
  			p = p.next;
  		}
 
- 		p.next = new StuffNode(x, null);
+ 		p.next = new IntNode(x, null);
  	}
 
  	public int size() {

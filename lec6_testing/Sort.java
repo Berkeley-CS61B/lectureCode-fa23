8a55dbc80a0062ebb39468b180d77a0ccfcbd741
peyrin <peyrin@berkeley.edu>
2024-09-10 17:14:42 -0700

    [lec06] blank - minor style fixes

--- lec6_testing/Sort.java
+++ lec6_testing/Sort.java
@@ -1,8 +1,8 @@
 package lec6_testing;
 
 public class Sort {
-	/** Sort the strings in alphabetical order. */
-	public static void sort(String[] x) {
-
+	/* Sort the words in alphabetical order. */
+	public static void sort(String[] words) {
+		return;
 	}
 }
--- lec6_testing/TestSort.java
+++ lec6_testing/TestSort.java
@@ -1,6 +1,6 @@
 package lec6_testing;
 
-// Import the Truth testing library that we use in 61B.
+/* Import the Truth testing library that we use in 61B. */
 import org.junit.jupiter.api.Test;
 import static com.google.common.truth.Truth.assertThat;
 
